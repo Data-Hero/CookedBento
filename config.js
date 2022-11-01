4cc61fa94200889d2322af747eb4afbf43c61343
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-11-01 17:27:12 +0100

    config.js update

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '', // Write here your API Key
-	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
